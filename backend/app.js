f87e47955c7bf4e2894fa180fdbf0f6dfbd88f70
ahmad ezzeir <ahmad.ezzeir@gmail.com>
2018-06-10 21:38:24 +0300

    finished get posts

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,36 @@
 const express = require('express');
 const app = express();
 
-app.use((req,res, next)=> {
-  console.log(new Date());
-  next();
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin','*');
+  res.setHeader('Access-Control-Allow-Header','Origin',
+    'Origin',
+    'Content-Type',
+    'Accept');
+    res.setHeader('GET','POST','PATCH','DELETE','OPTIONS');
+    next();
 });
 
-app.use((req,res, next)=> {
-  res.send('hello from express');
+
+app.use('/api/posts',(req,res, next) => {
+  //res.send('hello from express');
+  const posts = [
+    {
+      id: '1',
+      title:'title 1',
+      content:'content 1'
+    },
+    {
+      id: '2',
+      title:'title 2',
+      content:'content 3'
+    },
+
+  ];
+  res.status(200).json({
+      message:'fetch from service',
+      posts: posts
+  });
 });
 
 module.exports = app;
